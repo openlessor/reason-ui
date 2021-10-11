32937cdae015f0314f4fd1a3377d050869141a08
Brian Kaplan <brian@softwaredeployed.com>
2021-10-11 15:20:54 -0500

    bootstrap tailwind css

---
+++ src/Landing/Landing.bs.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var React = require("react");
+
+function str(prim) {
+  return prim;
+}
+
+function Landing(Props) {
+  return React.createElement("div", {
+              className: "h-screen justify-center flex items-center"
+            }, React.createElement("div", {
+                  className: "max-w-sm rounded overflow-hidden shadow-lg p-4"
+                }, React.createElement("div", {
+                      className: "px-6 py-4"
+                    }, React.createElement("div", {
+                          className: "font-bold text-xl mb-2"
+                        }, "Reserve Equipment"), React.createElement("p", {
+                          className: "text-gray-700 text-base"
+                        }, "Book an equipment reservation"))));
+}
+
+var make = Landing;
+
+exports.str = str;
+exports.make = make;
+/* react Not a pure module */
---
+++ src/Landing/Landing.re
@@ -0,0 +1,17 @@
+let str = React.string;
+//let logo: string = [%raw "require('./assets/logo.png')"];
+
+[@react.component]
+let make = () =>
+  <div className=[%tw "h-screen justify-center flex items-center"]>
+    <div className=[%tw "max-w-sm rounded overflow-hidden shadow-lg p-4"]>
+      <div className=[%tw "px-6 py-4"]>
+        <div className=[%tw "font-bold text-xl mb-2"]>
+          {"Reserve Equipment" |> str}
+        </div>
+        <p className=[%tw "text-gray-700 text-base"]>
+          {"Book an equipment reservation" |> str}
+        </p>
+    </div>
+  </div>
+</div>;
